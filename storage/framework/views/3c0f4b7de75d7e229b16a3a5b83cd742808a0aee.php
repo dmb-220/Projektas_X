44ab790e34d7239debbfcff351ac36d875c43446
DMB-220 <dmb2200@gmail.com>
2019-12-18 10:47:15 +0200

    Add files via upload

---
+++ storage/framework/views/3c0f4b7de75d7e229b16a3a5b83cd742808a0aee.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+    </div>
+</body>
+</html>
+<?php /**PATH C:\wamp64\www\Projektas_X\resources\views/layouts/app.blade.php ENDPATH**/ ?>
